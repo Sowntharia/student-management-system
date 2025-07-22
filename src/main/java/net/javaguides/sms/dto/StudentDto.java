d6425074bb2eb1762230686040c9b6e8ccf4eda3
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-22 15:57:47 +0200

    Refactor StudentDto to fix constructor issues and avoid duplication

--- src/main/java/net/javaguides/sms/dto/StudentDto.java
+++ src/main/java/net/javaguides/sms/dto/StudentDto.java
@@ -1,22 +1,39 @@
 package net.javaguides.sms.dto;
 
-public class StudentDto extends BasePersonDto {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class StudentDto {
 
     private Long id;
 
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+ // Constructors
     public StudentDto() {
-        super();
     }
 
     public StudentDto(Long id, String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
         this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
     public StudentDto(String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
+        this(null, firstName, lastName, email); 
+
     }
 
+    // Getters & Setters
     public Long getId() {
         return id;
     }
@@ -24,4 +41,29 @@ public class StudentDto extends BasePersonDto {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
