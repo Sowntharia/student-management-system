d99a9d15b729d5480c36ae3b06c079471f1b480e
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-22 18:32:36 +0200

    Improved the Code

--- src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -1,60 +1,12 @@
 package net.javaguides.sms;
 
-import net.javaguides.sms.dto.StudentDto;
-import net.javaguides.sms.entity.Student;
-import net.javaguides.sms.repository.StudentRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(StudentManagementSystemApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner run(StudentRepository studentRepository) {
-        return args -> {
-            try {
-                StudentDto student1 = new StudentDto();
-                student1.setFirstName("Ranesh");
-                student1.setLastName("Fadatare");
-                student1.setEmail("ranesh@gmail.com");
-
-                StudentDto student2 = new StudentDto();
-                student2.setFirstName("Sanjay");
-                student2.setLastName("Jadhav");
-                student2.setEmail("sanjay@gmail.com");
-
-                StudentDto student3 = new StudentDto();
-                student3.setFirstName("Tony");
-                student3.setLastName("Fadatare");
-                student3.setEmail("tony@gmail.com");
-
-                studentRepository.save(toEntity(student1));
-                studentRepository.save(toEntity(student2));
-                studentRepository.save(toEntity(student3));
-
-                logger.info("Sample student data initialized successfully.");
-            } catch (Exception e) {
-                logger.error("Error initializing student data", e);
-            }
-        };
-    }
-
-    // Manual DTO to Entity conversion
-    private Student toEntity(StudentDto dto) {
-        Student student = new Student();
-        student.setFirstName(dto.getFirstName());
-        student.setLastName(dto.getLastName());
-        student.setEmail(dto.getEmail());
-        return student;
     }
 }
