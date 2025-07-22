522d088d5b9666e54f537deb191aa51296fbef17
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-22 18:41:06 +0200

    Fixed StudentManagementSystemApplicationTest

--- src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTest.java
+++ src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class StudentManagementSystemApplicationTest {
+class StudentManagementSystemApplicationTest {
 
     @Test
     void contextLoads() {
