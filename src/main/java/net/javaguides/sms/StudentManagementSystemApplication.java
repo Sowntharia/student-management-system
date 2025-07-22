3d17d96077a6342f58c35530382b76ed4495c2e0
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-23 00:44:25 +0200

    Fix some issues in code

--- src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -1,12 +1,31 @@
 package net.javaguides.sms;
 
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(StudentRepository studentRepository) {
+        return args -> {
+            Student student1 = new Student("Ranesh", "Fadatare", "ranesh@gmail.com");
+            Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+            Student student3 = new Student("Tony", "Fadatare", "tony@gmail.com");
+
+            studentRepository.saveAll(Arrays.asList(student1, student2, student3));
+
+            System.out.println("Sample students saved successfully.");
+        };
     }
 }
