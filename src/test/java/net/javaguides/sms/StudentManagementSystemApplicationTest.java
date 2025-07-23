3d82fcab3b7467315c0ab73c4a1f1dd8d6dfa43a
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-23 19:53:10 +0200

    Fixed code in StudentManagementSystemApplicationTest

--- src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTest.java
+++ src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.given;
 
+@SuppressWarnings("removal")
 @ActiveProfiles("test") // Ensures application-test.properties is used
 @SpringBootTest
 class StudentManagementSystemApplicationTest {
