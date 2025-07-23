25e8739f2afe9547bd4b5b28d8044fd66cba1cea
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-23 18:15:04 +0200

    Fixed code in StudentManagementSystemApplication

--- src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -2,13 +2,10 @@ package net.javaguides.sms;
 
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.repository.StudentRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
-import java.util.logging.Logger;
 
 /**
  * Entry point of the Student Management System application.
@@ -16,22 +13,19 @@ import java.util.logging.Logger;
  */
 @SpringBootApplication
 public class StudentManagementSystemApplication {
-
-    private static final Logger logger = Logger.getLogger(StudentManagementSystemApplication.class.getName());
+    
+	private StudentRepository studentRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner run(StudentRepository studentRepository) {
-        return args -> {
+    public void run(String... args) throws Exception {
             Student student1 = new Student("Ranesh", "Fadatare", "ranesh@gmail.com");
             Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
             Student student3 = new Student("Tony", "Fadatare", "tony@gmail.com");
 
             studentRepository.saveAll(Arrays.asList(student1, student2, student3));
-            logger.info("Sample students saved successfully.");
-        };
     }
-}
+    }
+
