5381cbdcba84b2911fa38b6fb7e1f49579fdbb1d
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-25 09:02:00 +0200

    Fixed code in StudentManagementSystemApplicationTest

--- src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTest.java
+++ src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTest.java
@@ -1,25 +1,28 @@
-package net.javaguides.sms;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import net.javaguides.sms.entity.Student;
-import net.javaguides.sms.repository.StudentRepository;
-@ActiveProfiles("test")
-@SpringBootTest
-class StudentManagementSystemApplicationTests {
-
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @Test
-    void contextLoadsAndInitializesStudents() {
-        List<Student> students = studentRepository.findAll();
-        assertFalse(students.isEmpty()); // check that some students got saved
-    }
-}
+package net.javaguides.sms; 
+import java.util.List; 
+import org.junit.jupiter.api.Assertions; 
+import org.junit.jupiter.api.Test; 
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.boot.test.context.SpringBootTest; 
+import org.springframework.test.context.ActiveProfiles; 
+import static org.junit.jupiter.api.Assertions.assertFalse; 
+import net.javaguides.sms.entity.Student; 
+import net.javaguides.sms.repository.StudentRepository; 
+@ActiveProfiles("test") 
+@SpringBootTest 
+class StudentManagementSystemApplicationTest 
+{     
+	@Autowired     
+	private StudentRepository studentRepository;     
+	@Test     
+	void testMain() 
+	{         //Arrange & Act         
+		System.setProperty("spring.profiles.active", "test");         
+		StudentManagementSystemApplication.main(new String[]{});         //Assert         
+		Assertions.assertTrue(true);     }     
+	@Test     
+	void contextLoadsAndInitializesStudents() {         
+		List<Student> students = studentRepository.findAll();         
+		assertFalse(students.isEmpty()); // check that some students got saved     
+		}
+	}
