805f45f29ac48dc9b4340e670c873796fefa477a
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-27 01:03:16 +0200

    Add New File

--- src/test/java/net/javaguides/sms/controller/StudentRestControllerTest.java
+++ src/test/java/net/javaguides/sms/controller/StudentRestControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 
-public class StudentRestControllerTest {
+class StudentRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
