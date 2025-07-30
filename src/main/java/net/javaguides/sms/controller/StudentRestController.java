2a50971d8ad1e237c64f7d55cc65f7ae55e606ef
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-31 00:11:34 +0200

    Add new StudentRestController

---
+++ src/main/java/net/javaguides/sms/controller/StudentRestController.java
@@ -0,0 +1,21 @@
+package net.javaguides.sms.controller;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentRestController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student saved = studentService.saveStudent(student);
+        return ResponseEntity.ok(saved);
+    }
+}
