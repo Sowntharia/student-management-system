8da61c334911875d5f708ef2c336a67d1c8339df
sowntharia <sowntharia.vijayanand@edu.unifi.it>
2025-07-31 00:26:01 +0200

    test: update StudentControllerIntegrationTest for constructor injection

--- src/test/java/net/javaguides/sms/integration/StudentControllerIntegrationTest.java
+++ src/test/java/net/javaguides/sms/integration/StudentControllerIntegrationTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("testcontainers")
 @SpringBootTest
 @AutoConfigureMockMvc
-public class StudentControllerIntegrationTest extends BaseTestContainer{
+class StudentControllerIntegrationTest extends BaseTestContainer{
 
     @Autowired
     private MockMvc mockMvc;
